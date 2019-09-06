2863bcbdbc652f13159bf5d28288f7ebf0edad33
Caren B <carenpbriones@gmail.com>
2019-09-06 10:16:03 -0700

    Game displays questions one by one after 30 second intervals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
     function formatQuestion(q) {
         console.log(q.question);
-        $("#questions").append("<h6>" + q.question + "</h6>");
+        $("#question").html("<h6>" + q.question + "</h6>");
 
         for (var i = 0; i < q.answers.length; i++) {
-            $("#questions").append("<p>" + q.answers[i] + "</p>");
+            $("#answer-" + (i + 1)).html("<p>" + q.answers[i] + "</p>");
         }
     }
 
@@ -27,8 +27,54 @@ $(document).ready(function () {
 
     ];
 
-    for (var i = 0; i < questions.length; i++){
-        formatQuestion(questions[i]);
+    var intervalID;
+    var countdown = 30;
+    questionIndex = 0;
+
+
+    function askQuestion(){
+        clearInterval(intervalID);
+        // If all questions have not been cycled through
+        if(questionIndex !== questions.length){
+            intervalID = setInterval(countDownTime, 1000);
+
+            // Display question
+            formatQuestion(questions[questionIndex]);
+            questionIndex++;
+        } else{ // All questions have been cycled through; end game
+            clearInterval(intervalID);
+        }
     }
+
+    function countDownTime(){
+        countdown--;
+        $("#timer").text(countdown);
+
+        if (countdown === 0){
+            countdown = 30;
+            clearInterval(intervalID);
+            askQuestion();
+        }
+    }
+
+    // Starts game by displaying first question
+    $("#start").on("click", function(){
+        // Hides start button
+        $(this).css("display", "none");
+
+        // Asks first question
+        askQuestion();
+    })
+
+    
+
+    // User clicks start
+    // Timer for first question starts counting down
+        // User can either click an answer or not
+            // If user's answer is correct, show correct message, increment correct counter
+            // Else if user's answer is incorrect, show incorrect message, increment incorrect counter
+            // Else, unanswered and timer runs out; increment unanswered counter
+        // New question is displayed; continues until end of all questions
+        // End, display number of correct, incorrect, and unanswered questions
 
 })
